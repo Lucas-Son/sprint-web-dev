7cf2a267797fb0858709932372cb536065bd2794
Lucas-Son <162642950+Lucas-Son@users.noreply.github.com>
2024-06-15 20:24:34 +0900

    Update script.js
    Agora quando a pessoa clica no link compartilhado, o link a leva para a página de exemplo em vez da página de compartilhar

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function shareFacebook() {
-    var url = window.location.href;
+    var url = window.location.origin + window.location.pathname.replace('index.html', 'pagina2.html');
     window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url), '_blank');
 }
 
 function shareTwitter() {
-    var url = window.location.href;
+    var url = window.location.origin + window.location.pathname.replace('index.html', 'pagina2.html');
     var text = 'Confira este site incrível: ';
     window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(text) + '&url=' + encodeURIComponent(url), '_blank');
 }
