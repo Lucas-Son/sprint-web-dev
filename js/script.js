296322ed9aed5e17adc00141fdc53f26702dd2f3
Enzo Miletta <161778417+Enzo-Miletta@users.noreply.github.com>
2024-06-17 16:03:24 -0300

    Login e alert boxes feitas
    Login Padrão, Usuário: 1234
    Senha: 1234

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function shareFacebook() {
+    var url = window.location.origin + window.location.pathname.replace('index.html', 'pagina2.html');
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url), '_blank');
+}
+
+function shareTwitter() {
+    var url = window.location.origin + window.location.pathname.replace('index.html', 'pagina2.html');
+    var text = 'Confira este site incrível sobre a Formula E: ';
+    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(text) + '&url=' + encodeURIComponent(url), '_blank');
+}
